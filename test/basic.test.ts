85b3b2f52554d25a5b524e40d81be2a33032b56e
Teages <Teages@teages.xyz>
2024-01-02 17:53:00 +0800

    test: add basic case

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,14 +2,60 @@ import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
-describe('ssr', async () => {
+describe('Basic', async () => {
   await setup({
     rootDir: fileURLToPath(new URL('./fixtures/basic', import.meta.url)),
   })
 
-  it('renders the index page', async () => {
+  it('with no accept-language', async () => {
     // Get response to a server-rendered page with `$fetch`.
     const html = await $fetch('/')
-    expect(html).toContain('<div>basic</div>')
+    check(html, 'en-US')
+  })
+
+  it('with accept-language', async () => {
+    const html = await $fetch('/', {
+      headers: {
+        'Accept-Language': 'zh-CN',
+      },
+    })
+    check(html, 'zh-CN')
+  })
+
+  it('with accept-language and cookie', async () => {
+    const html = await $fetch('/', {
+      headers: {
+        'Accept-Language': 'zh-CN',
+        Cookie: 'locale=ja-JP',
+      },
+    })
+    check(html, 'ja-JP')
   })
 })
+
+function check(html: string, locale: string) {
+  if (locale === 'en-US') {
+    expect(html).toContain(`locale: ${locale}`)
+    expect(html).toContain(`general.hello: Hello`)
+    expect(html).toContain(`general.deep.hello: Deep: Hello`)
+    expect(html).toContain(`another.hello: Another Hello`)
+    expect(html).toContain(`another.deep.hello: Deep: Another Hello`)
+  }
+  else if (locale === 'zh-CN') {
+    expect(html).toContain(`locale: ${locale}`)
+    expect(html).toContain(`general.hello: 你好`)
+    expect(html).toContain(`general.deep.hello: 深层: 你好`)
+    expect(html).toContain(`another.hello: 另一个你好`)
+    expect(html).toContain(`another.deep.hello: 深层: 另一个你好`)
+  }
+  else if (locale === 'ja-JP') {
+    expect(html).toContain(`locale: ${locale}`)
+    expect(html).toContain(`general.hello: こんにちは`)
+    expect(html).toContain(`general.deep.hello: 次の層: こんにちは`)
+    expect(html).toContain(`another.hello: もう一度こんにちは`)
+    expect(html).toContain(`another.deep.hello: 次の層: もう一度こんにちは`)
+  }
+  else {
+    throw new Error(`Unknown locale: ${locale}`)
+  }
+}
--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,6 +1,25 @@
-<template>
-  <div>basic</div>
-</template>
-
-<script setup>
+<script setup lang="ts">
+const { loadedLocales, locales, locale } = useLocales()
 </script>
+
+<template>
+  <div>
+    <span>
+      locale: {{ locale }}
+    </span>
+    <ul id="messages">
+      <li verify="general.hello">
+        general.hello: {{ $t('general.hello') }}
+      </li>
+      <li verify="general.deep.hello">
+        general.deep.hello: {{ $t('general.deep.hello') }}
+      </li>
+      <li verify="another.hello">
+        another.hello: {{ $t('another.hello') }}
+      </li>
+      <li verify="another.deep.hello">
+        another.deep.hello: {{ $t('another.deep.hello') }}
+      </li>
+    </ul>
+  </div>
+</template>
